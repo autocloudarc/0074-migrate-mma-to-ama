be91483f444e1f176089b979fa86b532f7d0726a
devcloud0547 <devcloud0547@outlook.com>
2024-06-20 08:09:25 -0400

    provider(registration): skip [skip ci]

--- src/tfm/modules/plt/idy/providers.tf
+++ src/tfm/modules/plt/idy/providers.tf
@@ -1,8 +1,9 @@
 terraform {
   required_providers {
     azurerm = {
+      skip_provider_registration = true
       source = "hashicorp/azurerm"
-      version = "~>3.97.1"
+      version = "~>3.108.0"
       configuration_aliases = [
         azurerm.connectivity,
         azurerm.management,
