fe8e9892825ca5da2d95fec99df557c8e938d487
autocloudarc <autocloudarc@outlook.com>
2024-07-17 11:12:24 -0400

    ama(init): assignment review

--- src/tfm/modules/plt/idy/variables.tf
+++ src/tfm/modules/plt/idy/variables.tf
@@ -67,20 +67,22 @@ variable "umi_pol_id" {
   default = "/subscriptions/e25024e7-c4a5-4883-80af-9e81b2f8f689/resourceGroups/rgp-iac/providers/Microsoft.ManagedIdentity/userAssignedIdentities/umi-ama-mig-001"
 }
 
-# assignment_id_dcr_hybrid = "/providers/microsoft.management/managementgroups/org-id-platform/providers/microsoft.authorization/policyassignments/ama-arc-dcr"
 variable "ama_initiative_assignment" {
   type = map(string)
   description = "Enable Azure Monitor for VMs AMA"
   default = {
     name_dcr = "enable-ama-dcr"
-    name_hybrid_dcr = "enable-ama-hybrid-dcr"
     display_name = "Enable Azure Monitor for VMs with Azure Monitoring Agent(AMA)"
-    policy_set_def_id = "/providers/Microsoft.Authorization/policySetDefinitions/924bfe3a-762f-40e7-86dd-5c8b95eb09e6"
-    policy_set_hybrid_vm_def_id = "/providers/Microsoft.Authorization/policySetDefinitions/2b00397d-c309-49c4-aa5a-f0b2c5bc6321"
-    policy_set_hybrid_vm_def_display_name = "Enable Azure Monitor for Hybrid VMs with AMA"
-    effect = "DeployIfNotExists"
-    dcrResourceId = "/subscriptions/019181ad-6356-46c6-b584-444846096085/resourceGroups/rgp-idy/providers/Microsoft.Insights/dataCollectionRules/dcr"
     assignment_id_dcr = "/providers/microsoft.management/managementgroups/org-id-platform/providers/microsoft.authorization/policyassignments/enable-az-monitor-ama-dcr"
+    policy_set_def_id = "/providers/Microsoft.Authorization/policySetDefinitions/924bfe3a-762f-40e7-86dd-5c8b95eb09e6"
+
+    name_hybrid_dcr = "enable-ama-hybrid-dcr"
+    policy_set_hybrid_vm_def_display_name = "Enable Azure Monitor for Hybrid VMs with AMA"
+    assignment_id_dcr_hybrid = "/providers/microsoft.management/managementgroups/org-id-platform/providers/microsoft.authorization/policyassignments/ama-arc-dcr"
+    policy_set_hybrid_vm_def_id = "/providers/Microsoft.Authorization/policySetDefinitions/2b00397d-c309-49c4-aa5a-f0b2c5bc6321"
+    effect = "DeployIfNotExists"
+  
+    dcrResourceId = "/subscriptions/019181ad-6356-46c6-b584-444846096085/resourceGroups/rgp-idy/providers/Microsoft.Insights/dataCollectionRules/dcr"
     remediation_name_dcr = "remediate-az-monitor-ama-dcr"
   }
 }
